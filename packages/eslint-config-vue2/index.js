277086b83590fe1a7ce1c51ce2f01479c86f2b2a
liukai <107859668+kvuse@users.noreply.github.com>
2022-12-17 16:45:35 +0800

    eslint 验证添加关闭import/prefer-default-export

--- packages/eslint-config-vue2/index.js
+++ packages/eslint-config-vue2/index.js
@@ -21,7 +21,8 @@ module.exports = {
     'prefer-destructuring': ['error', { object: true, array: false }], // 允许数组通过下标取值
     'no-use-before-define': ['error', { functions: false }], // 允许function声明的函数在定义前使用
     'func-names': 'off', // 允许使用function声明匿名函数
-    'max-len': ['error', { code: 200 }], // 每一行最多200个字符
+    // 'max-len': ['error', { code: 200 }], // 每一行最多200个字符
+    'max-len': 'off', // 每一行最多200个字符
     'import/no-extraneous-dependencies': 'off', // 允许import开发依赖
     'vue/html-self-closing': 'off', // 允许空内容使用闭合标签
     'vue/singleline-html-element-content-newline': 'off', // 关闭单行标签的强制换行
@@ -39,6 +40,7 @@ module.exports = {
     'no-underscore-dangle': 'off', // 允许使用下划线开头的变量名
     'no-return-assign': ['error', 'except-parens'], // return 后如果有赋值语句，则必须加括号
     'no-param-reassign': 'off', // 允许修改方法中的参数
+    'import/prefer-default-export': 'off', // 模块只输出一个变量时，是否需要添加default
   },
   globals: {
     uni: true,
