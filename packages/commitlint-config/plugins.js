c6093a7ed7ccddac738a82636d8f2c27b0ab457a
liukai <107859668+kvuse@users.noreply.github.com>
2023-08-18 15:24:06 +0800

    feat: 规则提示的优化

--- packages/commitlint-config/plugins.js
+++ packages/commitlint-config/plugins.js
@@ -3,6 +3,9 @@ module.exports = [
     rules: {
       'type-empty': (parsed, when) => {
         const { type } = parsed;
+        const typeEnum = ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'config', 'chore', 'debug'];
+        if (!type) console.log('提交类型不能为空');
+        else if (!typeEnum.includes(type)) console.log('类型必须是[feat、fix、docs、style、refactor、test、revert、config、chore、debug]之一');
         return [
           when && type,
           'type may not be empty: 提交类型不能为空',
@@ -10,6 +13,7 @@ module.exports = [
       },
       'subject-empty': (parsed, when) => {
         const { type } = parsed;
+        if (!type) console.log('提交内容不能为空');
         return [
           when && type,
           'subject may not be empty: 提交内容不能为空',
@@ -17,7 +21,7 @@ module.exports = [
       },
       'subject-min-length': (parsed, when) => {
         const { subject = '' } = parsed;
-        console.log('subject: ', subject);
+        if (subject.length < 3) console.log('提交内容不能小于3个字符');
         return [
           when && subject.length >= 3,
           'subject must not be shorter than 3 characters: 提交内容不能小于3个字符',
@@ -27,6 +31,7 @@ module.exports = [
         const { subject } = parsed;
         const regex = /^[A-Za-z0-9]+$/;
         const hasNumberSequence = !regex.test(subject);
+        if (!hasNumberSequence) console.log('提交内容不能使用连续的数字或者字母');
         return [
           when && hasNumberSequence,
           '不能使用连续的数字或者字母',
