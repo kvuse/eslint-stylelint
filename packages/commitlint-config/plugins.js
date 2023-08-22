0a4f31799329a439c9bc492fd551857d2bf95fa3
liukai <107859668+kvuse@users.noreply.github.com>
2023-08-22 17:18:13 +0800

    chore: 优化commitlint验证提示

--- packages/commitlint-config/plugins.js
+++ packages/commitlint-config/plugins.js
@@ -1,29 +1,46 @@
+const typeEnum = ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'config', 'chore', 'debug'];
+function hasTypeEnum(parsed) {
+  const { type, header } = parsed;
+  const finalType = type || header?.split(': ')[0];
+  return typeEnum.includes(finalType);
+}
+
+function hasSpace(parsed) {
+  const { type, header } = parsed;
+  const finalType = type || header;
+  const isHasType = typeEnum.some((item) => finalType.includes(`${item}:`));
+  if (isHasType) return !/:\s/.test(finalType);
+  return false;
+}
+
 module.exports = [
   {
     rules: {
       'type-empty': (parsed, when) => {
-        const { type } = parsed;
-        const typeEnum = ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert', 'config', 'chore', 'debug'];
-        if (!type) console.log('提交类型不能为空');
-        else if (!typeEnum.includes(type)) console.log('类型必须是[feat、fix、docs、style、refactor、test、revert、config、chore、debug]之一');
+        const { type, header } = parsed;
+        const finalType = type || header?.split(': ')[0];
+        if (hasSpace(parsed)) console.log('[feat、fix、docs、style、refactor、test、revert、config、chore、debug] 冒号后要加空格');
+        if (!hasTypeEnum(parsed)) console.log('提交类型必须是[feat、fix、docs、style、refactor、test、revert、config、chore、debug]之一');
+        if (!finalType) console.log('提交类型不能为空');
         return [
           when && type,
           'type may not be empty: 提交类型不能为空',
         ];
       },
       'subject-empty': (parsed, when) => {
-        const { type } = parsed;
-        if (!type) console.log('提交内容不能为空');
+        const { subject, header } = parsed;
+        const finalSubject = subject || header?.split(': ')[1];
+        if (!finalSubject && hasTypeEnum(parsed)) console.log('提交内容不能为空');
         return [
-          when && type,
+          when && subject,
           'subject may not be empty: 提交内容不能为空',
         ];
       },
       'subject-min-length': (parsed, when) => {
         const { subject = '' } = parsed;
-        if (subject.length < 3) console.log('提交内容不能小于3个字符');
+        if (subject && subject?.length < 3) console.log('提交内容不能小于3个字符');
         return [
-          when && subject.length >= 3,
+          when && subject?.length >= 3,
           'subject must not be shorter than 3 characters: 提交内容不能小于3个字符',
         ];
       },
@@ -31,9 +48,9 @@ module.exports = [
         const { subject } = parsed;
         const regex = /^[A-Za-z0-9]+$/;
         const hasNumberSequence = !regex.test(subject);
-        if (!hasNumberSequence) console.log('提交内容不能使用连续的数字或者字母');
+        if (!hasNumberSequence && subject) console.log('提交内容不能使用连续的数字或者字母');
         return [
-          when && hasNumberSequence,
+          when && hasNumberSequence && subject,
           '不能使用连续的数字或者字母',
         ];
       },
