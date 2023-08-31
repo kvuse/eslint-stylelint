d4a990c641b29095a5bff7af546f3b8e6b981b62
liukai <107859668+kvuse@users.noreply.github.com>
2023-08-31 17:19:58 +0800

    chore: commitlint提交验证提示的优化

--- packages/commitlint-config/prepare.js
+++ packages/commitlint-config/prepare.js
@@ -2,6 +2,12 @@ const { exec } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
+const chalklog = {
+  success: (text) => console.log('\x1b[32m%s\x1b[0m', text),
+  error: (text) => console.log('\x1b[41m%s\x1b[0m', text),
+  warning: (text) => console.log('\x1b[31m%s\x1b[0m', text),
+};
+
 const copyFiles = ({
   readFile, writeFile, finishText, errorText,
 }) => {
@@ -10,27 +16,26 @@ const copyFiles = ({
 
   sourceStream.pipe(destStream)
     .on('finish', () => {
-      console.log(finishText);
+      chalklog.success(finishText);
     })
     .on('error', () => {
-      console.log(errorText);
-      console.error('请在根目录创建.commitlintrc.json');
+      chalklog.warning(errorText);
     });
 };
 
 const resolveDir = path.dirname(require.resolve('./'));
 
-const currentDirArr = resolveDir.split('/node_modules/');
-const currentDir = currentDirArr.length > 1 ? currentDirArr[0] : resolveDir.split('/packages/')[0];
+const isMac = resolveDir.includes('/');
+const currentDirArr = isMac ? resolveDir.split('/node_modules/') : resolveDir.split('\\node_modules\\');
+const currentDir = currentDirArr.length > 1
+  ? currentDirArr[0] : resolveDir.split(isMac ? '/packages/' : '\\packages\\')[0];
 const command = `cd ${currentDir} && husky install .husky`;
 
-exec(command, async (error, stdout, stderr) => {
+exec(command, async (error) => {
   if (error) {
-    console.error(`执行命令时出错：${error}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`命令错误：${stderr}`);
+    chalklog.error('执行命令时出错，请执行一下命令：');
+    chalklog.warning('cd node_modules/@kvuse/commitlint-config');
+    chalklog.warning('npm run preinstall');
     return;
   }
 
@@ -38,17 +43,17 @@ exec(command, async (error, stdout, stderr) => {
   if (!existFile) {
     exec(`cd ${currentDir} && npx husky add .husky/commit-msg 'npx --no -- commitlint --edit "$1"'`, (err) => {
       if (err) {
-        console.error('添加commit-msg失败，请手动添加');
+        chalklog.error('添加commit-msg失败，请手动添加');
       } else {
-        console.error('添加.husky成功');
+        chalklog.success('添加.husky成功');
       }
     });
     // 添加postinstall命令
     exec(`cd ${currentDir} && npm pkg set scripts.postinstall="cd node_modules/@kvuse/commitlint-config && npm run preinstall"`, (err) => {
       if (err) {
-        console.error('添加scripts失败，请在package.json添加postinstall命令');
+        chalklog.error('添加scripts失败，请在package.json添加prepare命令');
       } else {
-        console.error('添加scripts成功');
+        chalklog.success('添加scripts成功');
       }
     });
   }
